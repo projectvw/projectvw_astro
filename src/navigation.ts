9546031b993432d3cb0c0a20b9d998c54cd0dd08
Chris Hammond <github@christoc.com>
2024-12-03 18:41:02 +0000

    update favicons

--- src/navigation.ts
+++ src/navigation.ts
@@ -40,7 +40,6 @@ export const footerData = {
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="/assets/favicons/favicon.ico" alt="ProjectVW" loading="lazy"></img>
     Made by <a class="text-blue-600 underline dark:text-muted" href="https://www.chrishammond.com/"> Chris Hammond</a> · All rights reserved.
   `,
 };
